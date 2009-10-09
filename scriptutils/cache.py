be37aaa186fc70ed0ab0b5a6b7c91c3083a8b816
Jeremy Cantrell <jmcantrell@gmail.com>
2009-10-08 22:40:51 -0400

    Added to cache and made it a little more efficient

--- scriptutils/cache.py
+++ scriptutils/cache.py
@@ -1,24 +1,44 @@
 import os.path, pickle
+from glob import glob
 
 class Cache(object):
 
-    def __init__(self, directory):
+    def __init__(self, directory, **kwargs):
+        self.data = dict(**kwargs)
         self.directory = directory
-        if not os.path.isdir(self.directory):
-            os.makedirs(self.directory)
 
     def __delitem__(self, key):
-        os.remove(self._get_filename(key))
+        if key in self.data:
+            del(self.data[key])
+        fn = self._filename(key)
+        if os.path.isfile(fn):
+            os.remove(fn)
 
     def __getitem__(self, key):
-        filename = self._get_filename(key)
-        if os.path.isfile(filename):
-            return pickle.load(open(filename, 'rb'))
-        return None
+        if key not in self.data:
+            fn = self._filename(key)
+            if os.path.isfile(fn):
+                self.data[key] = pickle.load(open(fn, 'rb'))
+        return self.data[key]
 
     def __setitem__(self, key, value):
-        pickle.dump(value, open(self._get_filename(key), 'wb'))
+        self.data[key] = value
 
-    def _get_filename(self, key):
+    def _filename(self, key):
         return os.path.join(self.directory, '%s.pkl' % key)
 
+    def get(self, key, default=None):
+        try:
+            return self[key]
+        except KeyError:
+            return default
+
+    def set(self, key, value):
+        self[key] = value
+
+    def dump(self):
+        if not os.path.isdir(self.directory):
+            os.makedirs(self.directory)
+        for key, value in self.data.items():
+            if not value: continue
+            pickle.dump(value, open(self._filename(key), 'wb'))
