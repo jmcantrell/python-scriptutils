b6a622652d1b0654cd55ff2d21e856769797ae25
Jeremy Cantrell <jmcantrell@gmail.com>
2009-10-16 00:14:31 -0400

    Apparently OptionParser doesn't inherit from object

--- scriptutils/options.py
+++ scriptutils/options.py
@@ -1,11 +1,12 @@
 from optparse import OptionParser, OptionGroup, IndentedHelpFormatter
 
-class Options(OptionParser):
+class Options(object, OptionParser):
 
     def __init__(self, usage=None, width=None):
         if not usage: usage = 'Usage: %prog [options]'
         if not width: width = 35
-        super(Options, self).__init__(usage, add_help_option=None,
+        super(Options, self).__init__()
+        OptionParser.__init__(self, usage, add_help_option=None,
                 formatter=IndentedHelpFormatter(max_help_position=width))
 
     def add_option_group(self, name):
