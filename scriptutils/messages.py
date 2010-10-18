9983629406ffe224a8a3961465f6a31429ca2373
Jeremy Cantrell <jmcantrell@gmail.com>
2010-10-18 19:24:23 -0400

    Porting changes from bashful

--- scriptutils/messages.py
+++ scriptutils/messages.py
@@ -4,33 +4,56 @@ from unicodeutils import encode
 
 from . import ENCODING, TERMINAL, VERBOSE
 
-def color(c, message):
-    return c + message + TERMINAL.NORMAL
+def pack(t, text):
+    return ''.join([t, text, TERMINAL.NORMAL])
 
-def bold(message):
-    return TERMINAL.BOLD + message + TERMINAL.NORMAL
+def black(text):
+    return pack(TERMINAL.BLACK, text)
+
+def blue(text):
+    return pack(TERMINAL.BLUE, text)
+
+def green(text):
+    return pack(TERMINAL.GREEN, text)
+
+def cyan(text):
+    return pack(TERMINAL.CYAN, text)
+
+def red(text):
+    return pack(TERMINAL.RED, text)
+
+def magenta(text):
+    return pack(TERMINAL.MAGENTA, text)
+
+def yellow(text):
+    return pack(TERMINAL.YELLOW, text)
+
+def white(text):
+    return pack(TERMINAL.WHITE, text)
+
+def bold(text):
+    return pack(TERMINAL.BOLD, text)
+
+def tag_message(tag, message):
+    message = white(encode(condense(message), encoding=ENCODING))
+    return bold(' '.join([tag, message]))
 
 def info_message(message='All updates are complete.'):
-    message = condense(message)
-    return bold(encode(message, encoding=ENCODING))
+    return tag_message(blue('==>'), message)
 
 def info(check=False, **kwargs):
     if check and not VERBOSE: return
     print >>sys.stderr, info_message(**kwargs)
 
-def warn_message(**kwargs):
-    kwargs.setdefault('message', 'A warning has occurred.')
-    kwargs['message'] = color(TERMINAL.YELLOW, 'WARNING: %s' % kwargs['message'])
-    return info_message(**kwargs)
+def warn_message(message='A warning has occurred.'):
+    return tag_message(yellow('WARNING:'), message)
 
 def warn(check=False, **kwargs):
     if check and not VERBOSE: return
     print >>sys.stderr, warn_message(**kwargs)
 
-def error_message(**kwargs):
-    kwargs.setdefault('message', 'An error has occurred.')
-    kwargs['message'] = color(TERMINAL.RED, 'ERROR: %s' % kwargs['message'])
-    return info_message(**kwargs)
+def error_message(message='An error has occurred.'):
+    return tag_message(red('ERROR:'), message)
 
 def error(**kwargs):
     if check and not VERBOSE: return
--- scriptutils/options.py
+++ scriptutils/options.py
@@ -1,16 +1,8 @@
-from optparse import OptionParser, OptionGroup, IndentedHelpFormatter
+from argparse import ArgumentParser, ArgumentGroup, IndentedHelpFormatter
 
-class Options(object, OptionParser):
+class Arguments(ArgumentParser):
 
-    def __init__(self, usage=None, args=None, width=None):
-        if not usage: usage = 'Usage: %prog [options]'
-        if args: usage += ' %s' % args.strip()
+    def __init__(self, width=None, **kwargs):
         if not width: width = 45
-        object.__init__(self)
-        OptionParser.__init__(self, usage, add_help_option=None, formatter=IndentedHelpFormatter(max_help_position=width))
-        self.add_option('-h', '--help', action='help', help='Show this help message and exit.')
-
-    def add_option_group(self, name):
-        group = OptionGroup(self, name)
-        super(Options, self).add_option_group(group)
-        return group
+        super(Arguments, self).__init__(**kwargs, add_help_option=None, formatter=IndentedHelpFormatter(max_help_position=width))
+        self.add_argument('-h', '--help', action='help', help='Show this help message and exit.')
