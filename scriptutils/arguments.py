4106a9d4591f4be39464215fad09109fdd351889
Jeremy Cantrell <jmcantrell@gmail.com>
2010-10-18 21:51:22 -0400

    Changed arguments handling... again.

--- scriptutils/arguments.py
+++ scriptutils/arguments.py
@@ -1,8 +1,10 @@
-from argparse import ArgumentParser, ArgumentGroup, IndentedHelpFormatter
+from argparse import ArgumentParser, HelpFormatter
 
 class Arguments(ArgumentParser):
 
     def __init__(self, width=None, **kwargs):
-        if not width: width = 45
-        super(Arguments, self).__init__(**kwargs, add_help_option=None, formatter=IndentedHelpFormatter(max_help_position=width))
-        self.add_argument('-h', '--help', action='help', help='Show this help message and exit.')
+        def get_formatter(*args, **kwargs):
+            kwargs['max_help_position'] = width or 45
+            return HelpFormatter(*args, **kwargs)
+        kwargs['formatter_class'] = get_formatter
+        super(Arguments, self).__init__(**kwargs)
