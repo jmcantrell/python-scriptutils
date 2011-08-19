66691014e5a8ea4f071f313741258e7795a87aae
Jeremy Cantrell <jmcantrell@gmail.com>
2011-08-19 16:04:25 -0400

    changes to execute functions

--- scriptutils/execute.py
+++ scriptutils/execute.py
@@ -1,6 +1,23 @@
 import os, shlex, sys
 from subprocess import PIPE, Popen
 
+def split_command(command):
+    if isinstance(command, (list, tuple)):
+        return command
+    return shlex.split(command)
+
+def get_output(value):
+    if not value: return sys.stdout
+    if isinstance(value, (str, unicode)):
+        return open(value, 'w')
+    return value
+
+def getattr_safe(o, attr, default=None):
+    try:
+        return getattr(o, attr) or default
+    except AttributeError:
+        return default
+
 def in_directory(f, directory=None):
     """Decorator for running a function within a directory."""
     def new_f(*args, **kwargs):
@@ -13,6 +30,18 @@ def in_directory(f, directory=None):
             os.chdir(prev_cwd)
     return new_f
 
+def in_self_directory(m, directory=None):
+    """Decorator for running a function within a directory."""
+    def new_m(self, *args, **kwargs):
+        d = directory or getattr_safe(self, 'directory', os.getcwd())
+        prev_cwd = os.getcwd()
+        os.chdir(d)
+        try:
+            return m(self, *args, **kwargs)
+        finally:
+            os.chdir(prev_cwd)
+    return new_m
+
 @in_directory
 def system(command, **kwargs):
     kwargs.setdefault('stdout', sys.stdout)
@@ -21,10 +50,19 @@ def system(command, **kwargs):
 
 @in_directory
 def execute(command, **kwargs):
-    if not isinstance(command, (list, tuple)):
-        command = shlex.split(command)
     kwargs.setdefault('stdout', PIPE)
     kwargs.setdefault('stderr', PIPE)
-    p = Popen(command, **kwargs)
+    p = Popen(split_command(command), **kwargs)
+    o, e = p.communicate()
+    return p.returncode, o, e
+
+@in_directory
+def chain(commands, output=None, **kwargs):
+    stdin = None
+    for command in commands:
+        p = Popen(command, stdout=PIPE, stdin=stdin)
+        stdin = p.stdout
+    p.stdout = get_output(output)
     e, o = p.communicate()
     return p.returncode, o, e
+
